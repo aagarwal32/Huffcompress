b43a1b4b755b5a2380007f67da2da61e12f87b39
Arjun Agarwal <arjunagw02@gmail.com>
2023-12-09 13:29:08 -0500

    added getters/setters and bool fns to huffmanNode

--- huffmanNode.py
+++ huffmanNode.py
@@ -1,10 +1,29 @@
 # this python file contains the node class for huffcompress
-import heapq
 
-class HuffmanNode:
-    # initialize node with data symbol, frequency, and left and right NULL pointers
+class HNode:
+    # initialize node with symbol and frequency data
+    # set left and right pointers to None
     def __init__(self, symbol, frequency):
         self.__symbol = symbol
         self.__frequency = frequency
         self.__left = None
         self.__right = None
+        self.__parent = None
+
+    # getters
+    def getSymbol(self):
+        return self.__symbol
+    
+    def getFrequency(self):
+        return self.__frequency
+    
+    # setters
+    def setSymbol(self, symbol):
+        self.__symbol = symbol
+    
+    def setFrequency(self, frequency):
+        self.__frequency = frequency
+
+    bool = isRoot = lambda self: self.__parent == None
+    bool = isParent = lambda self: self.__left != None or self.__right != None
+    bool = isLeaf = lambda self: self.__left == None and self.__right == None
--- huffmanTree.py
+++ huffmanTree.py
@@ -1,10 +1,11 @@
 # this python file initializes the huffman tree and other huffcompress functions
-from huffmanNode import HuffmanNode
+from huffmanNode import HNode
+import heapq
 
 class HuffmanTree:
     # create huffman node with specified symbol and frequency
     def createHuffmanNode(self, symbol, frequency):
-        return HuffmanNode(symbol, frequency)
+        return HNode(symbol, frequency)
     
     def compress(self, inputStr):
         pass
